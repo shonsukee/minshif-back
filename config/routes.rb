43c6a05ce74cbc59febca1eb097426146cb859f1
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-01-28 22:57:04 +0900

    [Add]ユーザのtoken取得機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+	# 開発用のルーティング
+	get '/get_token', to: 'auth#get_token'
+	get '/get_access_token', to: 'auth#get_access_token'
+	get '/validate', to: 'auth#validate_access_token'
+	get '/fetch_events', to: 'auth#fetch_events'
+
 	namespace 'api' do
 		namespace 'v1' do
-			get "/sign_up", to: "sign_up#index"
-			post "/sign_up", to: "sign_up#create"
-
-			get "/login", to: "login#index"
-			post "/login", to: "login#create"
 		end
 	end
 end
