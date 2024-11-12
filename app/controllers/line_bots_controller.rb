d1b4dd53e9da7d677055732ba5b9713e4dcaa1fc
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-11-12 17:40:15 +0900

    [Add]メッセージ追加

--- app/controllers/line_bots_controller.rb
+++ app/controllers/line_bots_controller.rb
@@ -17,24 +17,41 @@ class LineBotsController < ApplicationController
 		end
 
 		events = client.parse_events_from(body)
+		message = {
+			type: 'text',
+			text: 'チャットしていただきありがとうございます！\nLINE Botはテキストのみの対応です😭\nご了承ください'
+		}
 		events.each do |event|
 			case event
 			when Line::Bot::Event::Message
 				case event.type
 				when Line::Bot::Event::MessageType::Text
-					message = {
-						type: 'text',
-						text: event.message['text']
-					}
-					client.reply_message(event['replyToken'], message)
-				when Line::Bot::Event::MessageType::Image
-					response = client.get_message_content(event.message['id'])
-					tf = Tempfile.open("content")
-					tf.write(response.body)
+					code = event.message['text']
+
+					if code.to_s == "ヘルプ"
+						message[:text] = "シフト通知をするLINE Botの設定方法を紹介します！\n\n1. アプリで整数4桁の認証コードを生成する\n2. 認証コードをコピーする\n3. LINE Botへ認証コードを送信する\n\n認証に成功すると次の午前9時からシフト通知を行います🚀"
+					else
+						# 既に登録済みの場合
+						# User テーブルから検索
+						if event['replyToken'].exist
+							message[:text] = "既に登録されているようです👀\nいつもご利用いただきありがとうございます！"
+
+						# 未登録の場合
+						# 数値の場合
+						elsif code.match?(/\A\d+\z/) && code.abs.to_s.size == 4
+							# DBから調査
+							message[:text] = "認証に成功しました🚀\nご利用いただきありがとうございます！"
+
+						else
+							# 整数４桁で入力するよう促す
+							message[:text] = "チャットしていただきありがとうございます！\nLINE Botは個別にチャットすることはできません😭\n\n使い方の詳細は「ヘルプ」とお問合せください"
+						end
+					end
 				end
 			end
 		end
 
+		client.reply_message(event['replyToken'], message)
 		head :ok
 	end
 
