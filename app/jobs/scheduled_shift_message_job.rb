3ac2fa3dd50e94d8acacff5c1be2994375d4f46c
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-11-16 12:28:17 +0900

    [Fix]スケジュール調整

--- app/jobs/scheduled_shift_message_job.rb
+++ app/jobs/scheduled_shift_message_job.rb
@@ -1,3 +1,5 @@
+require "time"
+
 class ScheduledShiftMessageJob < ApplicationJob
 	queue_as :default
 
@@ -5,13 +7,15 @@ class ScheduledShiftMessageJob < ApplicationJob
 		# 明日シフトに入っている人を抽出
 		tomorrow_shift_users = User.joins(:memberships)
 			.joins('INNER JOIN shifts ON memberships.id = shifts.membership_id')
+			.joins('INNER JOIN stores ON memberships.store_id = stores.id')
 			.where.not(line_user_id: nil)
 			.where(shifts: { shift_date: Date.tomorrow })
 			.distinct
-			.pluck(:line_user_id)
+			.pluck('users.line_user_id', 'shifts.start_time', 'shifts.end_time', 'stores.store_name')
 
-		tomorrow_shift_users.each do |line_user_id|
-			LineBotsController.new.send_shift_message(line_user_id)
+		jst_time = 9 * 60 * 60
+		tomorrow_shift_users.each do |line_user_id, start_time, end_time, store_name|
+			LineBotsController.new.send_shift_message(line_user_id, store_name, start_time + jst_time, end_time + jst_time)
 		end
 	end
 end
